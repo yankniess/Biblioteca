0f6d1eb52b8859056b630a30ac8ad0ae344e6f44
yankniess <you@example.com>
2022-11-03 18:42:25 -0300

    modified:   biblioteca/settings.py

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -139,7 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    BASE_DIR /"static",
+]
 
 # Enable WhiteNoise's GZip compression of static assets.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
